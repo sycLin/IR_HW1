1c0fec6a70d341203a3a0afff36658e07c38b8c6
sycLin <b01902044@ntu.edu.tw>
2016-04-06 02:19:05 +0800

    + first commit of my main source file
    basic function structure done.
    global variables and settings done.
    the function "set_up_from_argv()" is done.

---
+++ vsm.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python
+import sys
+import os
+from collections import defaultdict
+
+
+###################################
+# global variables for settings   #
+# (will be specified by sys.argv) #
+###################################
+
+MODEL_DIR = ""
+OUTPUT_FILE_NAME = ""
+QUERY_FILE_NAME = ""
+NTCIR_DIR = ""
+
+########################################
+# global variables for data            #
+# (will be constructed by the program) #
+########################################
+
+DOC_COUNT = 0
+VOCABULARY = set()
+POSTINGS_LISTS = defaultdict(dict) # dict of dict
+DOCUMENT_FREQUENCY = defaultdict(int) # dict of int
+DOC_LENGTHS = defaultdict(float) # dict of float
+
+#########################
+# helper functions area #
+#########################
+
+"""
+the main function to call
+"""
+def main():
+	set_up_from_argv()
+	pass
+
+"""
+to get global settings from sys.argv
+"""
+def set_up_from_argv():
+	global OUTPUT_FILE_NAME, QUERY_FILE_NAME, MODEL_DIR, NTCIR_DIR
+	# argv[1]: output file name
+	# argv[2]: query file name
+	# argv[3]: model dir
+	# argv[4]: NTCIR dir
+	if os.path.exists(sys.argv[1]):
+		OUTPUT_FILE_NAME = sys.argv[1]
+	else:
+		print >> sys.stderr, "not a valid output file name: %s" % sys.argv[1]
+	if os.path.exists(sys.argv[2]):
+		QUERY_FILE_NAME = sys.argv[2]
+	else:
+		print >> sys.stderr, "not a valid query file name: %s" % sys.argv[2]
+	if os.path.isdir(sys.argv[3]):
+		MODEL_DIR = sys.argv[3]
+	else:
+		print >> sys.stderr, "not a valid model dir: %s" % sys.argv[3]
+	if os.path.isdir(sys.argv[4]):
+		NTCIR_DIR = sys.argv[4]
+	else:
+		print >> sys.stderr, "not a valid NTCIR dir: %s" % sys.argv[4]
+
+"""
+to build the vocabulary from the file vocab.all
+"""
+def build_vocabulary():
+	pass
+
+"""
+to build the postings lists from the file inverted-file
+"""
+def build_postings():
+	pass
+
+
+"""
+to compute the document frequency for all terms
+"""
+def compute_document_frequencies():
+	pass
+
+"""
+to compute the lengths for every document
+"""
+def compute_document_lengths():
+	pass
+
+"""
+to calculate the importance of a term in a certain document
+"""
+def compute_importance():
+	pass
+
+"""
+to compute the inverse-document-frequency from document frequency
+"""
+def copmute_idf_from_df():
+	pass
+
+"""
+to compute the cosine similarity for two vectors
+"""
+def compute_cos_similarity():
+	pass
+
+
+if __name__ == "__main__":
+	main()
